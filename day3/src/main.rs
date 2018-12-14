581d8d17c13f45c79e329a4d8a96fda069a1497a
David Morcillo <david.morcillo@gmail.com>
2018-12-14 12:34:39 +0100

    Add day3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -18,15 +18,21 @@ fn main() -> io::Result<()> {
         fabrics.push(Fabric::new(&line.to_string()));
     }
 
-    let result = overlap_count(&fabrics);
+    let overlap = compute_overlaps(&fabrics);
+    let result = overlap_count(&overlap);
 
     println!("Total overlap count: {}", result);
+
+    let result = no_overlap_fabric(&fabrics, &overlap).unwrap();
+
+    println!("The fabric that doesn't overlap is: #{}", result);
 
     Ok(())
 }
 
 #[derive(Debug)]
 struct Fabric {
+    id: u32,
     x: u32,
     y: u32,
     width: u32,
@@ -38,12 +44,14 @@ impl Fabric {
         let re = Regex::new(r"#(.*) @ (.*),(.*): (.*)x(.*)").unwrap();
         let cap = re.captures(input).unwrap();
 
+        let id: u32 = cap[1].parse().unwrap();
         let x: u32 = cap[2].parse().unwrap();
         let y: u32 = cap[3].parse().unwrap();
         let width: u32 = cap[4].parse().unwrap();
         let height: u32 = cap[5].parse().unwrap();
 
         Fabric {
+            id,
             x,
             y,
             width,
@@ -52,7 +60,7 @@ impl Fabric {
     }
 }
 
-fn overlap_count(fabrics: &[Fabric]) -> usize {
+fn compute_overlaps(fabrics: &[Fabric]) -> HashMap<(u32, u32), u32> {
     let mut overlap: HashMap<(u32, u32), u32> = HashMap::new();
 
     for fabric in fabrics.iter() {
@@ -64,7 +72,28 @@ fn overlap_count(fabrics: &[Fabric]) -> usize {
         }
     }
 
+    overlap
+}
+
+fn overlap_count(overlap: &HashMap<(u32, u32), u32>) -> usize {
     overlap.values().filter(|value| *value > &1).count()
+}
+
+fn no_overlap_fabric(fabrics: &[Fabric], overlap: &HashMap<(u32, u32), u32>) -> Option<u32> {
+    'outer: for fabric in fabrics.iter() {
+        for i in fabric.x..fabric.x + fabric.width {
+            for j in fabric.y..fabric.y + fabric.height {
+                if let Some(count) = overlap.get(&(i, j)) {
+                    if *count > 1 {
+                        continue 'outer;
+                    }
+                }
+            }
+        }
+        return Some(fabric.id);
+    }
+
+    return None;
 }
 
 #[cfg(test)]
@@ -75,12 +104,14 @@ mod test {
     fn test_fabric_from_string() {
         let input = String::from("#1 @ 1,3: 4x4");
         let Fabric {
+            id,
             x,
             y,
             width,
             height,
         } = Fabric::new(&input);
 
+        assert_eq!(id, 1);
         assert_eq!(x, 1);
         assert_eq!(y, 3);
         assert_eq!(width, 4);
@@ -90,25 +121,57 @@ mod test {
     #[test]
     fn test_overlap_count() {
         let fabric_1 = Fabric {
+            id: 1,
             x: 1,
             y: 3,
             width: 4,
             height: 4,
         };
         let fabric_2 = Fabric {
+            id: 2,
             x: 3,
             y: 1,
             width: 4,
             height: 4,
         };
         let fabric_3 = Fabric {
+            id: 3,
             x: 5,
             y: 5,
             width: 2,
             height: 2,
         };
         let fabrics = vec![fabric_1, fabric_2, fabric_3];
-
-        assert_eq!(overlap_count(&fabrics), 4);
+        let overlap = compute_overlaps(&fabrics);
+        assert_eq!(overlap_count(&overlap), 4);
     }
+
+    #[test]
+    fn test_no_overlap_fabric() {
+        let fabric_1 = Fabric {
+            id: 1,
+            x: 1,
+            y: 3,
+            width: 4,
+            height: 4,
+        };
+        let fabric_2 = Fabric {
+            id: 2,
+            x: 3,
+            y: 1,
+            width: 4,
+            height: 4,
+        };
+        let fabric_3 = Fabric {
+            id: 3,
+            x: 5,
+            y: 5,
+            width: 2,
+            height: 2,
+        };
+        let fabrics = vec![fabric_1, fabric_2, fabric_3];
+        let overlap = compute_overlaps(&fabrics);
+        assert_eq!(no_overlap_fabric(&fabrics, &overlap).unwrap(), 3);
+    }
+
 }
